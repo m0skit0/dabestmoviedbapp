485ca8e08741c526fb96dee2982493194a13f235
m0skit0 <ametric@gmail.com>
2021-08-11 00:56:40 +0200

    Initial commit

---
+++ app/src/androidTest/java/org/m0skit0/android/dabestmoviedbapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package org.m0skit0.android.dabestmoviedbapp
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.m0skit0.android.dabestmoviedbapp", appContext.packageName)
+    }
+}
