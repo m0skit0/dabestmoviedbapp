6076ee29f2be2a35644eba9abb86f4362922e97c
m0skit0 <ametric@gmail.com>
2021-08-11 13:54:45 +0200

    Create Retrofit service for most popular TV shows API

---
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/retrofit/TopRatedTVShow.kt
@@ -0,0 +1,43 @@
+package org.m0skit0.android.dabestmoviedbapp.data.retrofit
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class TopRatedTVShows(
+    @JsonProperty("page")
+    val page: Int = 0,
+    @JsonProperty("results")
+    val topRatedTVShows: List<TopRatedTVShow> = emptyList(),
+    @JsonProperty("total_pages")
+    val totalPages: Int = 0,
+    @JsonProperty("total_results")
+    val totalResults: Int = 0
+)
+
+data class TopRatedTVShow(
+    @JsonProperty("backdrop_path")
+    val backdropPath: String = "",
+    @JsonProperty("first_air_date")
+    val firstAirDate: String = "",
+    @JsonProperty("genre_ids")
+    val genreIds: List<Int> = emptyList(),
+    @JsonProperty("id")
+    val id: Int = 0,
+    @JsonProperty("name")
+    val name: String = "",
+    @JsonProperty("origin_country")
+    val originCountry: List<String> = emptyList(),
+    @JsonProperty("original_language")
+    val originalLanguage: String = "",
+    @JsonProperty("original_name")
+    val originalName: String = "",
+    @JsonProperty("overview")
+    val overview: String = "",
+    @JsonProperty("popularity")
+    val popularity: Double = 0.0,
+    @JsonProperty("poster_path")
+    val posterPath: String = "",
+    @JsonProperty("vote_average")
+    val voteAverage: Double = 0.0,
+    @JsonProperty("vote_count")
+    val voteCount: Int = 0
+)
