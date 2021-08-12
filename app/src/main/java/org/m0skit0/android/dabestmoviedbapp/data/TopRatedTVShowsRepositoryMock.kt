1290129dd5a96a2e3029dd719a24f5c1d79c13bd
m0skit0 <ametric@gmail.com>
2021-08-12 15:57:34 +0200

    Created mock repository for top rated TV shows

---
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/TopRatedTVShowsRepositoryMock.kt
@@ -0,0 +1,38 @@
+package org.m0skit0.android.dabestmoviedbapp.data
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class TopRatedTVShowsRepositoryMock : TopRatedTVShowsRepository {
+
+    private val mockTVShows = listOf(
+        TVShow(
+            "somepath",
+            "The Expanse",
+            10.0,
+            "The Expanse",
+            "Best series ever",
+            listOf("Science-Fiction"),
+            "14/12/2015",
+            listOf("us"),
+            "en",
+            10.0,
+            1,
+        ),
+        TVShow(
+            "anotherpath",
+            "Breaking Bad",
+            8.5,
+            "Breaking Bad",
+            "5th season was not necessary",
+            listOf("Family"),
+            "20/01/2008",
+            listOf("us"),
+            "en",
+            10.0,
+            1,
+        ),
+    )
+
+    override fun topRatedTVShows(): Flow<List<TVShow>> = flow { emit(mockTVShows) }
+}
