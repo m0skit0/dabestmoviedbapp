31afaed269a3565755bd0d8a50cd5dcef10de29f
m0skit0 <ametric@gmail.com>
2021-08-12 21:50:03 +0200

    Switched from Kluent to KotlinTest dependency

--- app/src/test/java/org/m0skit0/android/dabestmoviedbapp/data/TestTopRatedTVShowsRepositoryImpl.kt
+++ app/src/test/java/org/m0skit0/android/dabestmoviedbapp/data/TestTopRatedTVShowsRepositoryImpl.kt
@@ -1,15 +1,14 @@
 package org.m0skit0.android.dabestmoviedbapp.data
 
+import io.kotlintest.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
-import org.amshove.kluent.shouldBe
 import org.junit.Before
 import org.junit.Test
-import org.m0skit0.android.dabestmoviedbapp.data.retrofit.TopRatedTVShowApi
 import org.m0skit0.android.dabestmoviedbapp.data.retrofit.TopRatedTVShowsApi
 import org.m0skit0.android.dabestmoviedbapp.data.retrofit.TopRatedTVShowsService
 
@@ -21,17 +20,25 @@ class TestTopRatedTVShowsRepositoryImpl {
     @MockK
     private lateinit var mockTVGenreMapper: TVGenreMapper
 
+    private val topRatedTVShowsRepositoryImpl: TopRatedTVShowsRepositoryImpl
+        get() = TopRatedTVShowsRepositoryImpl(
+            mockTopRatedTVShowsService,
+            mockTVGenreMapper
+        )
+
     @Before
     fun setup() {
         MockKAnnotations.init(this)
     }
 
     @Test
-    fun `when top rated tv shows is an empty list, should return an empty list`() {
-        every { mockTopRatedTVShowsService.topRatedTVShows() } returns flow { emit(TopRatedTVShowsApi()) }
+    fun `when top rated tv shows is an empty list, repo should return an empty list`() {
+        every { mockTopRatedTVShowsService.topRatedTVShows() } returns flow {
+            emit(TopRatedTVShowsApi())
+        }
         runBlocking {
-            mockTopRatedTVShowsService.topRatedTVShows().collect {
-                it.topRatedTVShows shouldBe emptyList<TopRatedTVShowApi>()
+            topRatedTVShowsRepositoryImpl.topRatedTVShows().collect {
+                it shouldBe emptyList()
             }
         }
     }
