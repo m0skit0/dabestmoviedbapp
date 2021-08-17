6992eb10b901d18bffba4137c27537e9743e49ee
m0skit0 <ametric@gmail.com>
2021-08-17 11:33:11 +0200

    Added API key as @Query parameter and added page for listing API

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/retrofit/TVGenreService.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/retrofit/TVGenreService.kt
@@ -2,8 +2,11 @@ package org.m0skit0.android.dabestmoviedbapp.data.retrofit
 
 import org.m0skit0.android.dabestmoviedbapp.BuildConfig
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TVGenreService {
-    @GET("genre/tv/list?api_key=${BuildConfig.API_KEY}")
-    suspend fun tvGenres(): TVGenresApi
+    @GET("genre/tv/list")
+    suspend fun tvGenres(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
+    ): TVGenresApi
 }
