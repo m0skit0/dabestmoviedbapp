9cc6f3463fab74832da9773234e3e9f79d60793c
m0skit0 <ametric@gmail.com>
2021-08-17 17:09:11 +0200

    Created layout and data model for tv show details

---
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetails.kt
@@ -0,0 +1,11 @@
+package org.m0skit0.android.dabestmoviedbapp.presentation.showdetails
+
+data class TVShowDetails(
+    val id: Long,
+    val poster: String,
+    val title: String,
+    val voteAverage: String,
+    val voteCount: String,
+    val genres: String,
+    val overview: String,
+)
