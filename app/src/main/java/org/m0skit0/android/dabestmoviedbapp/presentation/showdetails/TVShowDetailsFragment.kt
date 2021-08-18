10fec6f6a51b481b9fbd4e53e30672c297f09dce
m0skit0 <ametric@gmail.com>
2021-08-18 09:28:05 +0200

    Created details fragment and bound view

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsFragment.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsFragment.kt
@@ -1,55 +1,42 @@
-package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
+package org.m0skit0.android.dabestmoviedbapp.presentation.showdetails
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.m0skit0.android.dabestmoviedbapp.R
-import org.m0skit0.android.dabestmoviedbapp.databinding.FragmentTopRatedTvShowsBinding
-import org.m0skit0.android.dabestmoviedbapp.presentation.utils.hasReachedBottom
+import org.m0skit0.android.dabestmoviedbapp.databinding.FragmentTvShowDetailsBinding
 import org.m0skit0.android.dabestmoviedbapp.presentation.utils.invisible
 import org.m0skit0.android.dabestmoviedbapp.presentation.utils.toast
 import org.m0skit0.android.dabestmoviedbapp.presentation.utils.visible
 
 @AndroidEntryPoint
-class TopRatedTVShowsFragment : Fragment() {
+class TVShowDetailsFragment : Fragment() {
 
-    private val viewModel: TopRatedTVShowsViewModel by viewModels()
+    private val viewModel: TVShowDetailsViewModel by viewModels()
 
-    private lateinit var binding: FragmentTopRatedTvShowsBinding
+    private lateinit var binding: FragmentTvShowDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_top_rated_tv_shows, null)
+    ): View = inflater.inflate(R.layout.fragment_tv_show_details, null)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentTopRatedTvShowsBinding.bind(view).apply {
-            lifecycleOwner = this@TopRatedTVShowsFragment
-            topRatedRecycler.setupScrollListenerForNextPage()
+        binding = FragmentTvShowDetailsBinding.bind(view).apply {
+            lifecycleOwner = this@TVShowDetailsFragment
         }
         setupErrorListener()
         refresh()
-    }
-
-    private fun RecyclerView.setupScrollListenerForNextPage() {
-        addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                if (hasReachedBottom()) {
-                    refresh()
-                }
-            }
-        })
     }
 
     private fun setupErrorListener() {
@@ -63,9 +50,11 @@ class TopRatedTVShowsFragment : Fragment() {
     private fun refresh() {
         loading()
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.topRatedShows().let {
-                binding.topRatedRecycler updateWith it
-            }
+            arguments?.getLong(KEY_ID)?.let { id ->
+                viewModel.tvShowDetails(id).let {
+                    binding.tvShowDetails = it
+                }
+            } ?: toast(R.string.error_happened)
             loaded()
         }
     }
@@ -76,13 +65,14 @@ class TopRatedTVShowsFragment : Fragment() {
         }
     }
 
-    private infix fun RecyclerView.updateWith(list: List<TopRatedTVShowsItem>) {
-        (adapter as? TopRatedListAdapter)?.updateWith(list) ?: run { adapter = TopRatedListAdapter(list) }
-    }
-
     private fun loaded() {
         with(binding) {
             loading.invisible()
         }
+    }
+
+    companion object {
+        private const val KEY_ID = "id"
+        fun bundle(id: Long) = bundleOf(KEY_ID to id)
     }
 }
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
@@ -1,7 +1,12 @@
 package org.m0skit0.android.dabestmoviedbapp.presentation.showdetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
 import org.m0skit0.android.dabestmoviedbapp.domain.showdetails.TVShowDetailsUseCase
 import javax.inject.Inject
 
@@ -9,6 +14,11 @@ import javax.inject.Inject
 class TVShowDetailsViewModel @Inject constructor(
     private val tvShowDetailsUseCase: TVShowDetailsUseCase
 ) : ViewModel() {
+
+    val error: Flow<Boolean> = MutableStateFlow(false).shareIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(3000),
+    )
 
     suspend fun tvShowDetails(id: Long): TVShowDetailsPresentation =
         tvShowDetailsUseCase.tvShowDetails(id).toTVShowDetailsPresentation()
