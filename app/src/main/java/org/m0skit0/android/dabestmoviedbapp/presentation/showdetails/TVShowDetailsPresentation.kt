e83bc98a336822f45ab1f77ff035e1f685918d50
m0skit0 <ametric@gmail.com>
2021-08-18 16:12:08 +0200

    Implemented similar TV shows in view-model

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsPresentation.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsPresentation.kt
@@ -3,13 +3,13 @@ package org.m0skit0.android.dabestmoviedbapp.presentation.showdetails
 import org.m0skit0.android.dabestmoviedbapp.domain.showdetails.TVShowDetailsDomain
 
 data class TVShowDetailsPresentation(
-    val id: Long,
-    val poster: String,
-    val title: String,
-    val voteAverage: String,
-    val voteCount: String,
-    val genres: String,
-    val overview: String,
+    val id: Long = -1,
+    val poster: String = "",
+    val title: String = "",
+    val voteAverage: String = "",
+    val voteCount: String = "",
+    val genres: String = "",
+    val overview: String = "",
 )
 
 fun TVShowDetailsDomain.toTVShowDetailsPresentation() = TVShowDetailsPresentation(
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
@@ -1,18 +1,25 @@
 package org.m0skit0.android.dabestmoviedbapp.presentation.showdetails
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.m0skit0.android.dabestmoviedbapp.domain.showdetails.TVShowDetailsUseCase
+import org.m0skit0.android.dabestmoviedbapp.domain.similarshows.SimilarTVShowUseCase
 import org.m0skit0.android.dabestmoviedbapp.presentation.utils.common.ErrorViewModel
 import org.m0skit0.android.dabestmoviedbapp.presentation.utils.common.ErrorViewModelImpl
 import javax.inject.Inject
 
 @HiltViewModel
 class TVShowDetailsViewModel @Inject constructor(
-    private val tvShowDetailsUseCase: TVShowDetailsUseCase
+    private val tvShowDetailsUseCase: TVShowDetailsUseCase,
+    private val similarTVShowUseCase: SimilarTVShowUseCase,
 ) : ViewModel(), ErrorViewModel by ErrorViewModelImpl() {
+
+    private var currentId: Long = -1
+
+    private var nextSimilarShow: Long = -1
 
     private var _error = MutableStateFlow(false)
 
@@ -21,6 +28,21 @@ class TVShowDetailsViewModel @Inject constructor(
         setViewModelScope(viewModelScope)
     }
 
-    suspend fun tvShowDetails(id: Long): TVShowDetailsPresentation =
-        tvShowDetailsUseCase.tvShowDetails(id).toTVShowDetailsPresentation()
+    suspend fun tvShowDetails(id: Long): TVShowDetailsPresentation = run {
+        currentId = id
+        try {
+            loadSimilarShows(id)
+            tvShowDetailsUseCase.tvShowDetails(id).toTVShowDetailsPresentation().apply {
+                _error.value = false
+            }
+        } catch (e: Exception) {
+            Log.e("topRatedShows", "Error", e)
+            _error.value = true
+            TVShowDetailsPresentation()
+        }
+    }
+
+    private suspend fun loadSimilarShows(id: Long) {
+        nextSimilarShow = similarTVShowUseCase.similarTVShows(id).first().id
+    }
 }
