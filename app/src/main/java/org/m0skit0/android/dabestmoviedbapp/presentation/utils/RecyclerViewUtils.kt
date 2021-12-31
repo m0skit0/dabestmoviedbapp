887ca30a312b8eb8bd515b81ac29070b00dc1dd8
m0skit0 <ametric@gmail.com>
2021-12-31 19:32:27 +0100

    Clean up of unused code

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/utils/RecyclerViewUtils.kt
+++
@@ -1,6 +0,0 @@
-package org.m0skit0.android.dabestmoviedbapp.presentation.utils
-
-import androidx.recyclerview.widget.RecyclerView
-
-// Has reached bottom if we can't scroll down anymore
-fun RecyclerView.hasReachedBottom(): Boolean = !canScrollVertically(1)
