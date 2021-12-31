2d3ba54eae0ed50431fe6bd1e0980829517f5b31
m0skit0 <ametric@gmail.com>
2021-12-31 18:39:53 +0100

    Removed all UI elements

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/MainActivity.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/MainActivity.kt
@@ -1,12 +1,15 @@
 package org.m0skit0.android.dabestmoviedbapp.presentation
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import androidx.fragment.app.FragmentActivity
-import org.m0skit0.android.dabestmoviedbapp.R
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello")
+        }
     }
 }
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/OnTVShowClicked.kt
+++
@@ -1,5 +0,0 @@
-package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
-
-fun interface OnTVShowClicked {
-    fun onClicked(tvShow: TopRatedTVShowsItem)
-}
