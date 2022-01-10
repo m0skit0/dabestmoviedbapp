f98f7d0df0d8adac14dfb57c296cc6921430674b
m0skit0 <ametric@gmail.com>
2022-01-10 18:21:02 +0100

    Working with Compose

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/MainActivity.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/MainActivity.kt
@@ -2,13 +2,30 @@ package org.m0skit0.android.dabestmoviedbapp.presentation
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.m0skit0.android.dabestmoviedbapp.di.NAMED_TOP_TV_SHOWS_USE_CASE
+import org.m0skit0.android.dabestmoviedbapp.di.koin
+import org.m0skit0.android.dabestmoviedbapp.domain.toprated.TopTVShowsUseCase
+import org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist.PROFILE_NAME_TOP_SHOW_LIST
+import org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist.TopShowList
+import org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist.toTopRatedListingItem
+import org.m0skit0.android.dabestmoviedbapp.state.TopRatedState
+import org.m0skit0.android.dabestmoviedbapp.state.updateWithNextPage
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Progress()
+            MainComposable()
         }
 //        lifecycleScope.launch {
 //            topTVShowsUseCase(TopRatedState().updateWithNextPage()).fold({
@@ -23,3 +40,17 @@ class MainActivity : FragmentActivity() {
 //        }
     }
 }
+
+@Composable
+fun MainComposable() {
+    NavHost(navController = rememberNavController(), startDestination = PROFILE_NAME_TOP_SHOW_LIST) {
+        composable(PROFILE_NAME_TOP_SHOW_LIST) {
+            TopShowList(topShowsListing(koin().get(NAMED_TOP_TV_SHOWS_USE_CASE)).topRatedShows.map { it.toTopRatedListingItem() })
+        }
+    }
+}
+
+fun topShowsListing(topTVShowsUseCase: TopTVShowsUseCase): TopRatedState =
+    runBlocking(Dispatchers.IO) {
+        topTVShowsUseCase(TopRatedState().updateWithNextPage()).fold({TopRatedState()}) { it }
+    }
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
@@ -3,24 +3,42 @@ package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
-import org.m0skit0.android.dabestmoviedbapp.data.toprated.TopRatedTVShowData
+import org.m0skit0.android.dabestmoviedbapp.di.NAMED_TOP_TV_SHOWS_USE_CASE
+import org.m0skit0.android.dabestmoviedbapp.di.koin
+import org.m0skit0.android.dabestmoviedbapp.domain.toprated.TopTVShowsUseCase
+import org.m0skit0.android.dabestmoviedbapp.state.TopRatedState
+
+const val PROFILE_NAME_TOP_SHOW_LIST = "PROFILE_NAME_TOP_SHOW_LIST"
 
 @ExperimentalCoilApi
 @Composable
-fun TopShowItem(topRatedTVShowData: TopRatedTVShowData) {
+fun TopRatedTVShowsItem(topRatedTVShowData: TopRatedTVShowsItem) {
     Row {
         Image(
-            painter = rememberImagePainter(data = topRatedTVShowData.imagePath),
+            painter = rememberImagePainter(data = topRatedTVShowData.poster),
             contentDescription = null
         )
         Column {
-            Text(text = topRatedTVShowData.name)
-            Text(text = topRatedTVShowData.voteAverage.toString())
+            Text(text = topRatedTVShowData.title)
+            Text(text = topRatedTVShowData.voteAverage)
         }
     }
+}
 
+@ExperimentalCoilApi
+@Composable
+fun TopShowList(topRatedTVShows: List<TopRatedTVShowsItem>) {
+    val topRatedState = remember { koin().get<TopTVShowsUseCase>(NAMED_TOP_TV_SHOWS_USE_CASE) }
+    LazyColumn {
+        items(topRatedTVShows) { item ->
+            TopRatedTVShowsItem(topRatedTVShowData = item)
+        }
+    }
 }
