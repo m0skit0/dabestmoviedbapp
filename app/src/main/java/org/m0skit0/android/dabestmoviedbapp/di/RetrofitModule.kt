69c19042e881d96892367718c90d3a4800ffbfc3
m0skit0 <ametric@gmail.com>
2022-06-12 20:40:39 +0200

    Added configurable HTTP logging

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/di/RetrofitModule.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/di/RetrofitModule.kt
@@ -1,5 +1,8 @@
 package org.m0skit0.android.dabestmoviedbapp.di
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import org.m0skit0.android.dabestmoviedbapp.BuildConfig
 import org.m0skit0.android.dabestmoviedbapp.data.showdetails.TVShowDetailsService
@@ -10,10 +13,21 @@ import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 val retrofitModule = module {
+    factory<Interceptor> {
+        HttpLoggingInterceptor()
+    }
+    factory<OkHttpClient> {
+        OkHttpClient()
+            .newBuilder()
+            .apply {
+                if (BuildConfig.LOG_HTTP_CALLS) addInterceptor(get<Interceptor>())
+            }.build()
+    }
     single<Retrofit> {
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(JacksonConverterFactory.create())
+            .client(get())
             .build()
     }
     single<TopRatedTVShowsService> { get<Retrofit>().create(TopRatedTVShowsService::class.java) }
