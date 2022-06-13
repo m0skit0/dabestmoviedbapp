3a4efbd0277e79e7d0b63525e17828a204567175
m0skit0 <ametric@gmail.com>
2022-06-13 21:36:38 +0200

    Fixing text position for top rated shows listing

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
@@ -1,31 +1,38 @@
 package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import org.koin.androidx.compose.getViewModel
-import org.m0skit0.android.dabestmoviedbapp.presentation.Error
-import org.m0skit0.android.dabestmoviedbapp.presentation.Loading
-import org.m0skit0.android.dabestmoviedbapp.presentation.Progress
-import org.m0skit0.android.dabestmoviedbapp.presentation.Result
+import org.m0skit0.android.dabestmoviedbapp.presentation.*
 
 @ExperimentalCoilApi
 @Composable
 fun TopRatedTVShowsItem(topRatedTVShowData: TopRatedTVShowsItem) {
-    Row {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = rememberImagePainter(data = topRatedTVShowData.poster),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp),
         )
-        Column {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(text = topRatedTVShowData.title)
             Text(text = topRatedTVShowData.voteAverage)
         }
@@ -41,7 +48,7 @@ fun TopShowList(
     when (viewState.value) {
         is Loading -> Progress()
         is Error -> Error()
-        is Result<*> -> (viewState.value as? Result<List<TopRatedTVShowsItem>>)?.data?.let { items ->
+        is Result<*> -> (viewState.value.asResultTopRatedTVShowsItems())?.data?.let { items ->
             LazyColumn {
                 items(items) { item ->
                     TopRatedTVShowsItem(topRatedTVShowData = item)
@@ -51,23 +58,34 @@ fun TopShowList(
     }
 }
 
+private fun <T : ViewState> T.asResultTopRatedTVShowsItems(): Result<List<TopRatedTVShowsItem>>? =
+    this as? Result<List<TopRatedTVShowsItem>>
+
 @ExperimentalCoilApi
 @Preview
 @Composable
 fun TopRatedTVShowsItemPreview(
     topRatedTVShowData: TopRatedTVShowsItem = TopRatedTVShowsItem(
         id = 0L,
-        poster = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png",
+        poster = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_dark_color_272x92dp.png",
         title = "Google",
         voteAverage = "10.0"
     )
 ) {
-    Row {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = rememberImagePainter(data = topRatedTVShowData.poster),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp),
         )
-        Column {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(text = topRatedTVShowData.title)
             Text(text = topRatedTVShowData.voteAverage)
         }
