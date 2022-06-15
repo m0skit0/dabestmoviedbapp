c63bc9604f28b06c9fa262caf24bb5de6a52c9bc
m0skit0 <ametric@gmail.com>
2022-06-15 03:19:41 +0200

    Removed unused TopRatedStateHolder class

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedStateHolder.kt
+++
@@ -1,5 +0,0 @@
-package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
-
-import org.m0skit0.android.dabestmoviedbapp.state.TopRatedState
-
-data class TopRatedStateHolder(var topRatedState: TopRatedState = TopRatedState())
