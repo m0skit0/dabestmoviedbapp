b2a2a82bf7aa4caf330d743e3e4ab4f2e88df982
m0skit0 <ametric@gmail.com>
2022-06-18 00:47:45 +0200

    Cleaned everything

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/similarshows/SimilarTVShowData.kt
+++
@@ -1,7 +0,0 @@
-package org.m0skit0.android.dabestmoviedbapp.data.similarshows
-
-data class SimilarTVShowData(
-    val id: Long
-)
-
-fun SimilarTVShowDTO.toSimilarTVShowsData() = SimilarTVShowData(id = id.toLong())
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/similarshows/SimilarTVShowsView.kt
+++
@@ -1,5 +0,0 @@
-package org.m0skit0.android.dabestmoviedbapp.presentation.similarshows
-
-data class SimilarTVShowsView(
-    val similarShowsIds: List<Long> = emptyList()
-)
