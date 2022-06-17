8768fe07b062057c903131a4b06ede934a56e4f4
m0skit0 <ametric@gmail.com>
2022-06-17 13:40:37 +0200

    Added missing current show in similar show details

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/similarshows/SimilarTVShowsViewModel.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/similarshows/SimilarTVShowsViewModel.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import org.m0skit0.android.dabestmoviedbapp.domain.similarshows.SimilarTVShowsUseCase
-import org.m0skit0.android.dabestmoviedbapp.presentation.*
+import org.m0skit0.android.dabestmoviedbapp.presentation.Loading
+import org.m0skit0.android.dabestmoviedbapp.presentation.Result
+import org.m0skit0.android.dabestmoviedbapp.presentation.ViewState
+import org.m0skit0.android.dabestmoviedbapp.presentation.load
 import org.m0skit0.android.dabestmoviedbapp.state.SimilarShowsState
 
 class SimilarTVShowsViewModel(
@@ -22,7 +25,7 @@ class SimilarTVShowsViewModel(
                 viewStateResult = { state ->
                     state.similarShows
                     .map { it.id }
-                    .let { if (it.isEmpty()) Error else Result(it) }
+                    .let { Result(listOf(state.currentShowId) + it) }
                 }
             )
         }
