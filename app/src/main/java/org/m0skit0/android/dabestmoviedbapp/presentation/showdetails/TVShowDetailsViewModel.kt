551fe8d27a114f83bc1566f34a209e9c286ebeb9
m0skit0 <ametric@gmail.com>
2022-06-17 13:13:54 +0200

    Refactored top rated and tv show details load() function

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/showdetails/TVShowDetailsViewModel.kt
@@ -4,13 +4,8 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import org.m0skit0.android.dabestmoviedbapp.domain.showdetails.TVShowDetailsUseCase
-import org.m0skit0.android.dabestmoviedbapp.presentation.Error
-import org.m0skit0.android.dabestmoviedbapp.presentation.Loading
-import org.m0skit0.android.dabestmoviedbapp.presentation.Result
-import org.m0skit0.android.dabestmoviedbapp.presentation.ViewState
+import org.m0skit0.android.dabestmoviedbapp.presentation.*
 import org.m0skit0.android.dabestmoviedbapp.state.ShowDetailsState
 
 class TVShowDetailsViewModel(
@@ -19,19 +14,18 @@ class TVShowDetailsViewModel(
 ) : ViewModel() {
 
     private val _viewState: MutableState<ViewState> by lazy { mutableStateOf(Loading) }
-    val viewState: State<ViewState> by lazy { _viewState.apply { load() } }
-
-    private fun load() {
-        viewModelScope.launch {
-            tvShowDetailsUseCase(tvShowDetailsState).fold({
-                it.printStackTrace()
-                _viewState.value = Error
-            }){ state ->
-                _viewState.value = state.tvShowDetails
-                    ?.toTVShowDetailsPresentation()
-                    ?.let { Result(it) }
-                    ?: Error
-            }
+    val viewState: State<ViewState> by lazy {
+        _viewState.apply {
+            load(
+                useCase = { tvShowDetailsUseCase(tvShowDetailsState) },
+                viewState = _viewState,
+                viewStateResult = { state ->
+                    state.tvShowDetails
+                        ?.toTVShowDetailsPresentation()
+                        ?.let { Result(it) }
+                        ?: Error
+                }
+            )
         }
     }
 }
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedListViewModel.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedListViewModel.kt
@@ -4,13 +4,11 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import org.m0skit0.android.dabestmoviedbapp.domain.toprated.TopTVShowsUseCase
-import org.m0skit0.android.dabestmoviedbapp.presentation.Error
 import org.m0skit0.android.dabestmoviedbapp.presentation.Loading
 import org.m0skit0.android.dabestmoviedbapp.presentation.Result
 import org.m0skit0.android.dabestmoviedbapp.presentation.ViewState
+import org.m0skit0.android.dabestmoviedbapp.presentation.load
 import org.m0skit0.android.dabestmoviedbapp.state.TopRatedState
 
 class TopRatedListViewModel(
@@ -23,15 +21,14 @@ class TopRatedListViewModel(
     val viewState: State<ViewState> by lazy { _viewState.apply { load() } }
 
     private fun load() {
-        viewModelScope.launch {
-            topRatedUseCase(topRatedState).fold({
-                it.printStackTrace()
-                _viewState.value = Error
-            }) { state ->
+        load(
+            useCase = { topRatedUseCase(topRatedState) },
+            viewState = _viewState,
+            viewStateResult = { state ->
                 topRatedState = state
-                _viewState.value = Result(state.topRatedShows.map { it.toTopRatedListingItem() })
+                Result(state.topRatedShows.map { it.toTopRatedListingItem() })
             }
-        }
+        )
     }
 
     fun checkAndTriggerNextPageLoading(index: Int) {
