a5b87b14ad982036288173cc387e474f3f1ada4d
m0skit0 <ametric@gmail.com>
2022-06-18 11:40:11 +0200

    Added Retrofit module for Koin
    Added Retrofit module for Koin

---
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/di/RetrofitModule.kt
@@ -0,0 +1,17 @@
+package org.m0skit0.android.dabestmoviedbapp.di
+
+import org.koin.dsl.module
+import org.m0skit0.android.dabestmoviedbapp.BuildConfig
+import org.m0skit0.android.dabestmoviedbapp.data.toprated.TopRatedTVShowsService
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+val retrofitModule = module {
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+    }
+    single<TopRatedTVShowsService> { get<Retrofit>().create(TopRatedTVShowsService::class.java) }
+}
