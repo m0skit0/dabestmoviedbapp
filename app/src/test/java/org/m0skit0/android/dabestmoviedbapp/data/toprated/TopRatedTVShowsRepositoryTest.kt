bbca06790a03b07d90b96de4420c6bb1591ff684
m0skit0 <ametric@gmail.com>
2022-06-19 01:03:12 +0200

    Added more tests for TopRatedTVShowsRepository

--- app/src/test/java/org/m0skit0/android/dabestmoviedbapp/data/toprated/TopRatedTVShowsRepositoryTest.kt
+++ app/src/test/java/org/m0skit0/android/dabestmoviedbapp/data/toprated/TopRatedTVShowsRepositoryTest.kt
@@ -1,9 +1,14 @@
 package org.m0skit0.android.dabestmoviedbapp.data.toprated
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.maps.shouldBeEmpty
 import io.kotest.matchers.result.shouldBeFailureOfType
 import io.kotest.matchers.result.shouldBeSuccess
+import io.kotest.matchers.shouldBe
+import io.mockk.Called
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import org.m0skit0.android.dabestmoviedbapp.state.TopRatedState
 import java.io.IOException
@@ -45,6 +50,62 @@ class TopRatedTVShowsRepositoryTest : BehaviorSpec({
                     topRatedTVShowsService = mockTopRatedTVShowsService,
                     tvGenreService = mockTVGenreService
                 ).shouldBeSuccess()
+            }
+        }
+        When("Both services are success") {
+            And("Both services return empty lists") {
+                coEvery {
+                    mockTopRatedTVShowsService.topRatedTVShows(
+                        any(),
+                        any()
+                    )
+                } returns TopRatedTVShowsDTO()
+                coEvery { mockTVGenreService.tvGenres(any()) } returns TVGenresDTO()
+                Then("It should return a success with empty lists") {
+                    topRatedTVShowsRepository(
+                        state = TopRatedState(),
+                        topRatedTVShowsService = mockTopRatedTVShowsService,
+                        tvGenreService = mockTVGenreService
+                    ).shouldBeSuccess {
+                        it.topRatedShows.shouldBeEmpty()
+                        it.genreMappingCache.shouldBeEmpty()
+                    }
+                }
+            }
+            And("TV genres is not a empty list") {
+                val tvGenres = listOf(
+                    TVGenreDTO(id = 1, name = "foo"),
+                    TVGenreDTO(id = 2, name = "bar"),
+                )
+                val tvGenresMap = tvGenres.associate { it.id to it.name }
+                coEvery { mockTopRatedTVShowsService.topRatedTVShows(any(), any()) } returns TopRatedTVShowsDTO()
+                coEvery { mockTVGenreService.tvGenres(any()) } returns TVGenresDTO(tVGenres = tvGenres)
+                Then("It should call tvGenreService and fill state with it if cache is empty") {
+                    topRatedTVShowsRepository(
+                        state = TopRatedState(),
+                        topRatedTVShowsService = mockTopRatedTVShowsService,
+                        tvGenreService = mockTVGenreService
+                    ).shouldBeSuccess {
+                        it.genreMappingCache shouldBe tvGenresMap
+                    }
+                    coVerify(exactly = 1) {
+                        mockTVGenreService.tvGenres(any())
+                    }
+                }
+                Then("It should not call tvGenreService if cache is not empty") {
+                    topRatedTVShowsRepository(
+                        state = TopRatedState(
+                            genreMappingCache = tvGenresMap
+                        ),
+                        topRatedTVShowsService = mockTopRatedTVShowsService,
+                        tvGenreService = mockTVGenreService
+                    ).shouldBeSuccess {
+                        it.genreMappingCache shouldBe tvGenresMap
+                    }
+                    coVerify(exactly = 1) {
+                        mockTVGenreService.tvGenres(any()) wasNot Called
+                    }
+                }
             }
         }
     }
