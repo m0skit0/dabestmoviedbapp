030e59815b277d64c265e79304104bbdd0fe3890
m0skit0 <ametric@gmail.com>
2022-06-19 18:42:57 +0200

    Added TV show details Retrofit service
    Added TV show details Retrofit service

---
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/data/showdetails/TVShowDetailsService.kt
@@ -0,0 +1,14 @@
+package org.m0skit0.android.dabestmoviedbapp.data.showdetails
+
+import org.m0skit0.android.dabestmoviedbapp.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TVShowDetailsService {
+    @GET("tv/{id}")
+    suspend fun tvShowDetails(
+        @Path("id") id: Long,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+    ): TVShowDetailsDTO
+}
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/di/RetrofitModule.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/di/RetrofitModule.kt
@@ -2,6 +2,7 @@ package org.m0skit0.android.dabestmoviedbapp.di
 
 import org.koin.dsl.module
 import org.m0skit0.android.dabestmoviedbapp.BuildConfig
+import org.m0skit0.android.dabestmoviedbapp.data.showdetails.TVShowDetailsService
 import org.m0skit0.android.dabestmoviedbapp.data.toprated.TVGenreService
 import org.m0skit0.android.dabestmoviedbapp.data.toprated.TopRatedTVShowsService
 import retrofit2.Retrofit
@@ -16,4 +17,5 @@ val retrofitModule = module {
     }
     single<TopRatedTVShowsService> { get<Retrofit>().create(TopRatedTVShowsService::class.java) }
     single<TVGenreService> { get<Retrofit>().create(TVGenreService::class.java) }
+    single<TVShowDetailsService> { get<Retrofit>().create(TVShowDetailsService::class.java) }
 }
