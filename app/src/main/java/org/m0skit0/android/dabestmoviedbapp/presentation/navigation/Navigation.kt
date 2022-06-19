7b4d1b6c5976dc76e158520319dc464f120bb30b
m0skit0 <ametric@gmail.com>
2022-06-19 18:34:59 +0200

    Added navigation from top list to detail

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/navigation/Navigation.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/navigation/Navigation.kt
@@ -2,13 +2,21 @@ package org.m0skit0.android.dabestmoviedbapp.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.pager.ExperimentalPagerApi
+import org.m0skit0.android.dabestmoviedbapp.presentation.showdetails.TVShowDetailItem
 import org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist.TopShowList
 
 private const val ROUTE_TOP_TV_SHOWS = "topShows"
+private const val ROUTE_DETAIL_TV_SHOW_BASE = "tvShowDetail"
+private const val ROUTE_DETAIL_TV_SHOW_ID = "id"
+private const val ROUTE_DETAIL_TV_SHOW = "$ROUTE_DETAIL_TV_SHOW_BASE/{$ROUTE_DETAIL_TV_SHOW_ID}"
+
+private fun NavHostController.navigateToDetailsProvider(): (Long) -> Unit = { id -> navigate("$ROUTE_DETAIL_TV_SHOW_BASE/$id") }
 
 @ExperimentalPagerApi
 @ExperimentalCoilApi
@@ -16,7 +24,16 @@ private const val ROUTE_TOP_TV_SHOWS = "topShows"
 fun NavHostController.SetupNavHost() {
     NavHost(navController = this, startDestination = ROUTE_TOP_TV_SHOWS) {
         composable(ROUTE_TOP_TV_SHOWS) {
-            TopShowList()
+            TopShowList(
+                navigateToDetailsProvider()
+            )
+        }
+        composable(
+            route = ROUTE_DETAIL_TV_SHOW,
+            arguments = listOf(navArgument(ROUTE_DETAIL_TV_SHOW_ID) { type = NavType.LongType })
+        ) { backStackEntry ->
+            val tvShowId = backStackEntry.arguments?.getLong(ROUTE_DETAIL_TV_SHOW_ID) ?: 0
+            TVShowDetailItem(tvShowId)
         }
     }
 }
--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/topratedlist/TopRatedComposable.kt
@@ -3,6 +3,7 @@ package org.m0skit0.android.dabestmoviedbapp.presentation.topratedlist
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -14,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,10 +24,10 @@ import coil.compose.rememberImagePainter
 import org.koin.androidx.compose.getViewModel
 import org.m0skit0.android.dabestmoviedbapp.presentation.process
 
-
 @ExperimentalCoilApi
 @Composable
 fun TopShowList(
+    navigateToDetails: (id: Long) -> Unit,
     topRatedListViewModel: TopRatedListViewModel = getViewModel(),
 ) {
     val viewState = remember { topRatedListViewModel.viewState }
@@ -33,7 +35,10 @@ fun TopShowList(
         LazyColumn {
             itemsIndexed(topRatedTVShows) { index, item ->
                 topRatedListViewModel.checkAndTriggerNextPageLoading(index)
-                TopRatedTVShowsItem(topRatedTVShowData = item)
+                TopRatedTVShowsItem(
+                    navigateToDetails = navigateToDetails,
+                    topRatedTVShowData = item
+                )
             }
         }
     }
@@ -42,11 +47,17 @@ fun TopShowList(
 @ExperimentalCoilApi
 @Composable
 fun TopRatedTVShowsItem(
+    navigateToDetails: (id: Long) -> Unit,
     topRatedTVShowData: TopRatedTVShowView
 ) {
     TopListItemCard {
         Row(
             verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = { navigateToDetails(topRatedTVShowData.id) }
+                )
+            }
         ) {
             with(topRatedTVShowData) {
                 TopListItemPosterImage(posterUrl = poster)
