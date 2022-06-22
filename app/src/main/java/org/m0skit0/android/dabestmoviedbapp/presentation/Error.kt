d0e7f2b5c034767baf45cc5aad0f3b449c7a154f
m0skit0 <ametric@gmail.com>
2022-06-22 16:06:00 +0200

    Added error snackbar

--- app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/Error.kt
+++ app/src/main/java/org/m0skit0/android/dabestmoviedbapp/presentation/Error.kt
@@ -1,10 +1,13 @@
 package org.m0skit0.android.dabestmoviedbapp.presentation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,10 +16,40 @@ import org.m0skit0.android.dabestmoviedbapp.R
 @Preview
 @Composable
 fun Error() {
+    ErrorSnackbar(onTryAgain = { /*TODO*/ })
+}
+
+@Composable
+private fun ErrorSnackbar(
+    onTryAgain: () -> Unit,
+) {
+    SnackbarBox {
+        Snackbar(
+            action = {
+                ActionButtons(
+                    onTryAgain = onTryAgain,
+                )
+            }
+        ) {
+            Text(text = stringResource(R.string.error_happened))
+        }
+    }
+}
+
+@Composable
+private fun SnackbarBox(snackbar: @Composable BoxScope.() -> Unit) {
     Box(
-        contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(stringResource(id = R.string.error))
+        contentAlignment = BottomCenter,
+        content = snackbar
+    )
+}
+
+@Composable
+private fun ActionButtons(
+    onTryAgain: () -> Unit,
+) {
+    Button(onClick = onTryAgain) {
+        Text(text = stringResource(R.string.try_again))
     }
 }
